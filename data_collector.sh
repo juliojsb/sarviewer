d1b00974a010e06cbb71e9bbe26548b65e7d4c3f
juliojsb <jsb.inform@gmail.com>
2017-02-04 19:45:01 +0100

    Fixed skipping data error when launching plotter.sh too soon

--- data_collector.sh
+++ data_collector.sh
@@ -63,9 +63,12 @@ else
 	echo "Total time to collect data -> ${total_time} seconds"
 	echo ">>>Collecting data..."
 	echo ">>>Please wait until data collection is completed..."
-	echo "You can abort this script with Ctrl+C, but have in mind not all the data will stop being collected when you cancel it."
+	echo "You can abort this script with Ctrl+C, but have in mind the data will stop being collected when you cancel it."
 	echo "You will also need to manually launch script plotter.sh to generate the graphs."
 	sleep $total_time
+
+	# Added additional sleep of 5 seconds to avoid "warning: Skipping data file with no valid points"
+	sleep 5
 
 	# Call plotter.sh to generate the graphs
 	./plotter.sh
