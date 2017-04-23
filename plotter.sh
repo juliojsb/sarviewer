7456b2a54a601bc34f52f40ba82314e5a82f0f17
juliojsb <jsb.inform@gmail.com>
2017-04-23 20:50:18 +0200

    Added GRAPH_GENERATOR variable to plotter.sh

--- plotter.sh
+++ plotter.sh
@@ -4,17 +4,21 @@
 # Author        :Julio Sanz
 # Website       :www.elarraydejota.com
 # Email         :juliojosesb@gmail.com
-# Description   :Script to generate gnuplot graphs in the folder graphs/ of this repository
+# Description   :Script to generate graphs in the folder graphs/ of this repository
 # Dependencies  :sar,gnuplot
 # Usage         :1)Give executable permissions to script -> chmod +x plotter.sh
 #                2)Execute script -> ./plotter.sh
 # License       :GPLv3
 #
 
+# This variable can be "gnuplot" or "matplotlib" (Python) depending on how you want to generate the graphs
+# By default, gnuplot
+GRAPH_GENERATOR="gnuplot"
+
 if [ $# -ne 0 ];then
 	echo "This script doesn't accept parameters"
-else
-	cd plotters
+elif [ "$GRAPH_GENERATOR" == "gnuplot" ];then
+	cd plotters/gnuplot
 	gnuplot loadaverage.gplot
 	gnuplot cpu.gplot
 	gnuplot ram.gplot
@@ -22,4 +26,15 @@ else
 	gnuplot iotransfer.gplot
 	gnuplot proc.gplot
 	gnuplot contextsw.gplot
+elif [ "$GRAPH_GENERATOR" == "matplotlib" ];then
+	cd plotters/matplotlib
+	python loadaverage.py
+	python cpu.py
+	python ram.py
+	python swap.py
+	python iotransfer.py
+	python proc.py
+	python contextsw.py
+else
+	echo "Variable GRAPH_GENERATOR must be \"gnuplot\" or \"matplotlib\", please check plotter.sh"
 fi
